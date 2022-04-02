b1c6568178bbfcf4eef5cc5718aeb839ace3740b
bastis4 <galia.amelina@yandex.ru>
2022-04-02 18:03:18 +0300

    Fix method's name

--- JobFinder/Program.cs
+++ JobFinder/Program.cs
@@ -22,7 +22,7 @@ namespace JobFinder
             var repository = new VacancyRepository(_connectionString);
 
             var manager = new VacancyManager(repository, telegram, apiClient);
-            manager.InsertOrUpdate(allVacancies);
+            manager.SaveAndSend(allVacancies);
             Console.WriteLine("All done");
         }
     }
